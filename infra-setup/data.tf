8bc563522b9dcc5ef85014f8310f2af5b642c7ae
sathish <sathishcitmech@gmail.com>
2025-12-14 05:58:32 +0000

    infra setup

--- infra-setup/data.tf
+++ infra-setup/data.tf
@@ -4,12 +4,12 @@ data "aws_availability_zones" "available" {
 
 data "aws_caller_identity" "current" {}
 
-data "aws_ami" "amazon_linux" {
-  most_recent = true
+# data "aws_ami" "amazon_linux" {
+#   most_recent = true
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-  }
+#   filter {
+#     name   = "name"
+#     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+#   }
 
-}
+# }
